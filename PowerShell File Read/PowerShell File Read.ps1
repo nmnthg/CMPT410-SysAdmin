cc6c35bbd9d8fd77315bfd500f14c1eb68388b6d
namhoang1 <nam.hoang1@marist.edu>
2021-01-11 13:05:53 -0500

    PowerShell File Read assignment

---
+++ PowerShell File Read/PowerShell File Read.ps1
@@ -0,0 +1,20 @@
+﻿$depts = Get-Content C:\Users\John\Desktop\Departments.txt
+$emps = Get-Content C:\Users\John\Desktop\Names.txt
+
+$des = @()
+
+foreach($d in $depts){
+    if($d.Split(",")[1] -eq "Security Department"){
+        $des+=$d.Split(",")[0]
+    }
+}
+
+foreach($u in $des){
+    foreach($e in $emps){
+        if($u -eq $e.split(",")[2]){
+            $out = $null
+            $out = $e.split(",")[0] + "," + $e.split(",")[1]
+            $out >> C:\Users\John\Desktop\SEC_EMP.txt.txt
+        }
+    }
+}
